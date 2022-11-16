04c6a3c8a98f0a917faf561493da4ccbb24b7f8f
Diego Oliveira da Silva <diegolinkk@gmail.com>
2022-11-16 06:05:20 -0300

    implmementação de árvore binária

---
+++ 07-binary-tree/support.py
@@ -0,0 +1,40 @@
+#escrevendo uma árvore conforme estrutura abaixo
+
+#        '+'
+#       /   \
+#     'a'   '*'
+#           /  \
+#         'b'  '-'
+#              /  \
+#            '/'  'e'
+#           /  \
+#         'c'  'd'
+#
+#Em expressão matemática podemos definir a árvore acima
+#  (a + (b * ((c/d) - e)))
+#
+from tree import BinaryTree, Node
+
+tree = BinaryTree()
+n1 = Node('a')
+n2 = Node('+')
+n3 = Node('*')
+n4 = Node('b')
+n5 = Node('-')
+n6 = Node('/')
+n7 = Node('e')
+n8 = Node('c')
+n9 = Node('d')
+
+tree.root = n2
+n2.left = n1
+n2.right = n3
+
+n3.left = n4
+n3.right = n5
+
+n5.left = n6
+n5.right = n7
+
+n6.left = n8
+n6.right = n9
